37828f78272db6a5104581e7542cae5eef716500
The-sad-philosopher <shinudonney@tuta.io>
2021-01-29 17:10:32 +0530

    🚚 Move theme.ts to its own directory

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './components/App';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
-import theme from './theme';
+import theme from './themes';
 
 ReactDOM.render(
   <React.StrictMode>
