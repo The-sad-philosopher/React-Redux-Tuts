c3db3dd001ab99a128a8bacc2164a36493448fc0
The-sad-philosopher <shinudonney@tuta.io>
2021-01-30 01:05:58 +0530

    💄 Conditionally render SongDetails based on User feedback

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import { Flex } from '@chakra-ui/react';
 import * as React from 'react';
+import SongDetails from '../SongDetails/SongDetails';
 import SongList from '../SongList';
 
 export const App: React.FC = () => {
   return (
     <Flex minH="100vh" px="5rem">
       <SongList />
+      <SongDetails />
     </Flex>
   );
 };
--- src/components/SongDetails/SongDetails.tsx
+++ src/components/SongDetails/SongDetails.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { Box, Flex, Icon, Text } from '@chakra-ui/react';
+import { GiMusicalNotes } from 'react-icons/gi';
 import { connect } from 'react-redux';
 import { Song } from '../../actions';
 import { State } from '../../reducers';
@@ -8,8 +10,36 @@ interface SongDetailsProps {
 }
 
 const SongDetails: React.FC<SongDetailsProps> = ({ song }) => {
-  console.debug(song);
-  return <div>SongDetails</div>;
+  const details =
+    song != null ? (
+      <>
+        <Text fontSize="4xl" fontWeight="bold" mb="1.3rem">
+          <Icon as={GiMusicalNotes} mr="1.5rem" />
+          {song.title}
+        </Text>
+        <Text fontSize="1.2rem" color="gray.300">
+          Duration: {song.duration}
+        </Text>
+      </>
+    ) : (
+      <Text fontSize="3xl">
+        <Icon as={GiMusicalNotes} mr="1.5rem" />
+        Select a song
+      </Text>
+    );
+  return (
+    <Flex w="full" justifyContent="center" alignItems="center">
+      <Box
+        w="60%"
+        p="5rem"
+        borderRadius="lg"
+        border="1px"
+        borderColor="gray.200"
+      >
+        {details}
+      </Box>
+    </Flex>
+  );
 };
 
 const mapStateToProps = (state: State) => ({ song: state.selectedSong });
